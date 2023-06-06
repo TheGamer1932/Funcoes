968f2e84cc29e2efa13f6c6e713a19e0cb678a46
Tiago Ferreira da Silva <132114628+TheGamer1932@users.noreply.github.com>
2023-06-05 23:46:31 -0300

    Terceira Questão.js

---
+++ Terceira Questão.js
@@ -0,0 +1,10 @@
+function média_de_gols_por_partida()    {
+var part = Number(prompt("Digite o total de partidas: "))
+var gol  = Number(prompt("Coloque o total de gols: ")) {
+
+    medica = gol/part
+}
+média_de_gols_por_partida()
+
+console.log(`sua media de gols foi igual a ${medica} por partida`)
+}
