a34b5f0874179e9fe3373e197141c9819017603f
Tiago Ferreira da Silva <132114628+TheGamer1932@users.noreply.github.com>
2023-06-05 23:45:16 -0300

    Segunda Questão.js

---
+++ Segunda Questão.js
@@ -0,0 +1,15 @@
+function operações_aritméticas()  {
+    var num1 = Number(prompt("Digite o primeiro número: "))
+    var num2 =  Number(prompt("Digite o segundo número: "))
+    
+    //operações
+    //return (
+        adição = num1 + num2,
+        subtração = num1 - num2,
+        multiplicação = num1 * num2,
+        divisão = num1 / num2
+    //  );
+}
+operações_aritméticas()
+
+console.log('O resultado da sua soma é: ${adição},Sua subtração é igual a: ${subtração},Sua multiplicação é: ${multiplicação},E a sua divisão é: {divisão},')
