d1123f581bd2bc6685982a42e539cec38e55145c
Tiago Ferreira da Silva <132114628+TheGamer1932@users.noreply.github.com>
2023-06-05 23:47:31 -0300

    Quinta Questão.js

---
+++ Quinta Questão.js
@@ -0,0 +1,13 @@
+function distância_entre_dois_pontos(x1,y1,x2,y2) {
+    
+    var x1 = Number(prompt("Digite a coordenada x1: "))
+    var y1 = Number(prompt("Digite a coordenada y1: "))
+    var x2 = Number(prompt("Digite a coordenada x2: "))
+    var y2 = Number(prompt("Digite a coordenada y2: "))
+
+    d=((x1-x2)**2)+((y1-y2)**2)
+
+}
+distância_entre_dois_pontos()
+
+console.log(Match.sqrt(d))
